39247ffdbc1737fd348c98a04a8b5845e07061ea
Cameron Keenan <cameronrkeenan@gmail.com>
2020-09-09 16:16:46 +1200

    worked on channel views

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -2,3 +2,8 @@ var userAvatar = function(event) {
 	var image = document.getElementById('userAvaOutput');
 	image.src = URL.createObjectURL(event.target.files[0]);
 };
+
+var channelAvatar = function(event) {
+	var image = document.getElementById('channelAvaOutput');
+	image.src = URL.createObjectURL(event.target.files[0]);
+}
